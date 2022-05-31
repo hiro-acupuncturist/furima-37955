6e5ede98762faf075dc2eb1df95b9e2608998756
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-05-31 20:11:50 +0900

    ベーシック認証の解決

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,11 @@ class ApplicationController < ActionController::Base
 
   private
 
-  #   def basic_auth
-  #     authenticate_or_request_with_http_basic do |username, password|
-  #     username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-  #      end
-  #  end
+    def basic_auth
+      authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+       end
+   end
 
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == 'hiro' && password == '5155'
-    end
-  end
+  
 end
