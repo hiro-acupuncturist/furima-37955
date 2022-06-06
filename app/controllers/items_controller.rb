f95897d934533c939180c5e6c30a646bf79dabec
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-06 19:40:15 +0900

    各バリデーションの設定、ビューファイルにプルダウンの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,25 @@
 class ItemsController < ApplicationController
   def index
+
   end
 
   def new
     @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :instructions, :price, :deliveryaddress_id, :category_id, :quality_id, :deliverycharg_id, :deliveryday_id, :user_id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,20 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  belongs_to :deliveryaddress
+  belongs_to :category_id
+  belongs_to :quality_id
+  belongs_to :deliverycharg
+  belongs_to :deliveryday
+
+  validates :deliveryaddress, presence: true, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :category_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :quality_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliverycharg,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :deliveryday,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+
+  validates :name,           presence: true
+  validates :instructions,   presence: true
+  validates :price,          presence: true
+
 end
