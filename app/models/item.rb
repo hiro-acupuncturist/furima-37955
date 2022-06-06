8ec67d1d3cb376990fd135b8d453d45a552cb1b4
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-06 20:09:01 +0900

    商品価格の上限下限の設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,19 +2,20 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :deliveryaddress
-  belongs_to :category_id
-  belongs_to :quality_id
+  belongs_to :category
+  belongs_to :quality
   belongs_to :deliverycharg
   belongs_to :deliveryday
 
-  validates :deliveryaddress, presence: true, numericality: { other_than: 0 , message: "can't be blank"}
-  validates :category_id,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :quality_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :deliverycharg,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :deliveryday,     presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  with_options presence: true do
+    validates :deliveryaddress,  numericality: { other_than: 0 , message: "can't be blank"}
+    validates :category_id,      numericality: { other_than: 1 , message: "can't be blank"}
+    validates :quality_id,       numericality: { other_than: 1 , message: "can't be blank"}
+    validates :deliverycharg,    numericality: { other_than: 1 , message: "can't be blank"}
+    validates :deliveryday,      numericality: { other_than: 1 , message: "can't be blank"}
 
-  validates :name,           presence: true
-  validates :instructions,   presence: true
-  validates :price,          presence: true
-
+    validates :name           
+    validates :instructions 
+    validates :price,            numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, format: { with: /\A[0-9]+\z/ }
+  end
 end
