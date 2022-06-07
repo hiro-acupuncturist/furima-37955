83388fc2c16ccde88974412b3af2a70626923316
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-07 15:39:30 +0900

    価格、手数料、利益の計算

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
          validates :password,        format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります' } 
 
          with_options presence: true do
-          validates :nickname        
-          validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
-          validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
-          validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
-          validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
-          validates :date_of_birth   
+            validates :nickname        
+            validates :first_name,       format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
+            validates :last_name,        format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: '全角ひらがな、全角カタカナ、漢字で入力して下さい' }
+            validates :first_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
+            validates :last_name_kana,   format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナで入力して下さい' }
+            validates :date_of_birth   
          end
 end
