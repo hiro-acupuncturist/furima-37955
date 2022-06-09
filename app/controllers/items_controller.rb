58e2be8920c882843198561eb861807424152b0e
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-09 21:41:29 +0900

    ログインしていても、他ユーザーの編集はできない

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show ] 
+  
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -13,7 +13,6 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-
   def create
     @item = Item.new(item_params)
     if @item.save
@@ -23,10 +22,25 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :instructions, :price, :deliveryaddress_id, :category_id, :quality_id,
                                  :deliverycharg_id, :deliveryday_id).merge(user_id: current_user.id)
   end
+
 end
