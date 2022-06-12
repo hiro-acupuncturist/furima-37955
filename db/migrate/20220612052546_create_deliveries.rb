91e1d7987c3b6f08bf3f828f7413250ff8292541
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-12 14:36:36 +0900

    デリバリーモデル作成、マイグレ

---
+++ db/migrate/20220612052546_create_deliveries.rb
@@ -0,0 +1,17 @@
+class CreateDeliveries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :deliveries do |t|
+
+      t.integer     :deliveryaddress_id  ,null: false
+      t.string      :postcod             ,null: false
+      t.string      :municipalitids      ,null: false
+      t.string      :address             ,null: false
+      t.string      :building         #建物名　任意の値
+      t.string      :tel                 ,null: false
+      t.references  :purchaserecord      ,null: false , foreign_key: true
+
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_08_064650) do
+ActiveRecord::Schema.define(version: 2022_06_12_052546) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,19 @@ ActiveRecord::Schema.define(version: 2022_06_08_064650) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "deliveries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "deliveryaddress_id", null: false
+    t.string "postcod", null: false
+    t.string "municipalitids", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "tel", null: false
+    t.bigint "purchaserecord_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["purchaserecord_id"], name: "index_deliveries_on_purchaserecord_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -76,6 +89,7 @@ ActiveRecord::Schema.define(version: 2022_06_08_064650) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "deliveries", "purchaserecords"
   add_foreign_key "items", "users"
   add_foreign_key "purchaserecords", "items"
   add_foreign_key "purchaserecords", "users"
