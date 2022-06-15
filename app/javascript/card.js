ae67c2f243ec1a0225d3ba070dfb80c9def99bf6
hiro-acupuncturist <0zv66j3g326379v@gmail.com>
2022-06-15 18:01:45 +0900

    プリベントデフォルトの意味！！

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -2,7 +2,7 @@ const pay = () => {
   Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
-    // e.preventDefault();
+    e.preventDefault();
 
     const formResult = document.getElementById("charge-form");
     const formData = new FormData(formResult);
@@ -13,6 +13,7 @@ const pay = () => {
       exp_month:      formData.get("purchaserecord_delivery[exp_month]"),
       exp_year:  `20${formData.get("purchaserecord_delivery[exp_year]")}`,
     };
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
